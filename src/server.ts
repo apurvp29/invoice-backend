f48f5c2e9c37a41b21bce1acad9256a201f0a0d6
apurv patel <apurv.fluxbyte@gmail.com>
2024-03-19 12:06:24 +0530

    update node.js.yml, server.ts

--- src/server.ts
+++ src/server.ts
@@ -27,7 +27,10 @@ const jwtCheck = auth({
   tokenSigningAlg: "RS256",
 });
 
-app.use(cors());
+app.use(cors({
+  origin: true,
+  credentials: true,
+}));
 app.use(express.json());
 app.use("/api/images", express.static("templates"));
 app.use(jwtCheck);
