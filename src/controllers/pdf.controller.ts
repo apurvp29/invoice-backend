ee531ced073eab07e08411b02a0c13f4da224c18
apurvp29 <155609230+apurvp29@users.noreply.github.com>
2024-03-20 11:18:39 +0530

    Update pdf.controller.ts

--- src/controllers/pdf.controller.ts
+++ src/controllers/pdf.controller.ts
@@ -6,10 +6,10 @@ import { SuccessResponse } from "../config/constant";
 export const createPdf = async (req: Request, res: Response) => {
   console.log("1. inside create-pdf function")
   const bodyValue = req.body;
-  // const pdf = await generatePdf(bodyValue);
+  const pdf = await generatePdf(bodyValue);
   console.log("createpdf funtion complete .1")
-  // res.contentType("application/pdf");
+  res.contentType("application/pdf");
   return res
         .status(StatusCodes.OK)
-        .send(SuccessResponse("Data fetched", StatusCodes.OK, "pdf created."));
+        .send(SuccessResponse("Data fetched", StatusCodes.OK, pdf));
 };
