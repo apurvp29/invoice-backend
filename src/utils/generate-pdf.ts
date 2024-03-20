fbce25c0447057a8afc93ca93c47ebf36999f932
apurv patel <apurv.fluxbyte@gmail.com>
2024-03-20 11:55:45 +0530

    Handle: Path issue for production

--- src/utils/generate-pdf.ts
+++ src/utils/generate-pdf.ts
@@ -5,11 +5,15 @@ import * as fs from "fs-extra";
 
 const compile = async function (templateName: any, data: any) {
   console.log("3. inside compile fetching index.hbs");
-  const filePath = path.join(process.cwd(), "..", "templates", `${templateName}.hbs`);
+  const filePath = path.join(
+    process.cwd(),
+    "..",
+    "templates",
+    `${templateName}.hbs`
+  );
   console.log("filepath", filePath);
   try {
     const html = await fs.readFile(filePath, "utf-8");
-    console.log("return compile 3.");
     return hbs.compile(html)(data);
   } catch (err) {
     console.error("Error reading file:", err);
@@ -18,7 +22,7 @@ const compile = async function (templateName: any, data: any) {
 };
 
 export const generatePdf = async (data: any) => {
-  console.log("2. inside generatePdf function");
+  console.log("2. inside generatePdf function---called generatePdf");
   const browser = await puppeteer.launch({
     headless: true,
     executablePath: "/usr/bin/chromium-browser",
@@ -27,17 +31,25 @@ export const generatePdf = async (data: any) => {
 
   const content = await compile("index", data);
   await page.setContent(content, { waitUntil: "domcontentloaded" });
+  const pdfFolderDir = path.join(
+    process.cwd(),
+    "..",
+    "pdf",
+    `invoice_${data["invoiceNumber"]}.pdf`
+  );
+  console.log({ pdfFolderDir });
   const options = {
     format: "A4" as PaperFormat,
-    path: `./pdf/invoice_${data["invoiceNumber"]}.pdf`,
+    path: pdfFolderDir,
     printBackground: true,
   };
   try {
-    console.log("return generatepdf 2.");
-    return await page.pdf(options);
+    console.log("return generatepdf 2.--calling try");
+    const buffer = await page.pdf(options);
+    console.log("Buffer response", { buffer });
+    return buffer;
   } catch (err) {
-    console.log("return generatepdf 2. error");
-    console.error("Error generating PDF:", err);
+    console.log("error generatePdf 2--calling error", { err });
     throw err;
   } finally {
     await browser.close();
